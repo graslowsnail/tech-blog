e85a9119ae63174f8c53a6f28dbc9a58eb4ba40e
graslowsnail <graslowsnail@gmail.,com>
2022-11-17 23:28:27 -0600

    doop

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
 
-      host: "the-tech-bloggg.herokuapp.com",
+      host: "localhost",
       dialect: "mysql",
       port: 3306,
     });
